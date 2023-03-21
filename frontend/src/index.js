32b143278d757dfc4df3956e0f60e1d196e440ee
Blake Gassman <51721851+gblake31@users.noreply.github.com>
2023-03-21 19:23:58 -0400

    Added my name to home page to test heroku

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>Blake is here</h1>
     <App />
   </React.StrictMode>
 );
