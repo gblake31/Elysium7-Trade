f24d8a2713d57b860b19725e7e238d0593ac9475
Owen1Campbell <ocampbell@knights.ucf.edu>
2023-04-11 18:48:11 -0400

    added account management button to home

---
+++ mobile/app/screens/AccountManagement.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function AccountManagement(props) {
+    return (
+        <View>
+            <Text>H</Text>
+        </View>
+    );
+}
+
+export default AccountManagement;
--- mobile/app/screens/Home.js
+++ mobile/app/screens/Home.js
@@ -1,12 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import Product from '../components/Product';
+import { useRouter } from "expo-router";
 
 function Home(props) {
+    const router = useRouter();
     return (
         <View style={styles.home}>
             <Text style={styles.title}>Website Home Page</Text>
             <Product itemName={'Keyboard'} price={5} />
             <Product itemName={'Mouse'} price={10} />
+            <Button 
+                title='Manage Account'
+                onPress={() => router.replace('./AccountManagement')} 
+            />
         </View>
     );
 }
