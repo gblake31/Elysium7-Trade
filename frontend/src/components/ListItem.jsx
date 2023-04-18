9a300691a40aab2250dca14e35d1604044026a33
Blake Gassman <51721851+gblake31@users.noreply.github.com>
2023-04-18 10:20:27 -0400

    CSS on ItemList

--- frontend/src/components/ListItem.jsx
+++ frontend/src/components/ListItem.jsx
@@ -17,10 +17,9 @@ function ListItem(props) {
     // console.log(props.sellerid);
     return (
         <div id = 'card' onClick = {props.inventory ? editItem : clickItem}>
-            <h3 id='name'>{props.item.itemname}</h3>
             <img className='listItemImg' src = {props.item.image}></img>
+            <h3 id='name'>{props.item.itemname}</h3>
             <h3 id = 'price'>${props.item.price}</h3>
-            <p>{props.item.description}</p>
         </div>
     );
 }
